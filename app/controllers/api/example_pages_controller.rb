f1570a13258777e90fdbd8fe1df982a4accf35ee
brian <riceb53@gmail.com>
2021-02-21 11:13:35 -0500

    add second route

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,10 @@
 class Api::ExamplePagesController < ApplicationController
   def hello_action
+                  # user sees
     render json: {message: "helloooooo"}
+  end
+
+  def goodbye_action
+    render json: {messag: "adios"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do
+  # verb  path (insomnia)  controller     method in the controller
     get "/hello_path" => "example_pages#hello_action"
+    get "/goodbye_path" => "example_pages#goodbye_action"
   end
 end
