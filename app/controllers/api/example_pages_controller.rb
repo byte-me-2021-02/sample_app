3f0841aaf1154d931289427ec657d499d7c5912b
brian <riceb53@gmail.com>
2021-02-21 11:53:57 -0500

    move logic from view to controller

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,7 @@
 class Api::ExamplePagesController < ApplicationController
   def hello_action
+    @current_time = Time.now.strftime("%b %d, %Y %^B")
+    @message = "helloooooo"
                   # user sees
     render 'hello.json.jb'
   end
